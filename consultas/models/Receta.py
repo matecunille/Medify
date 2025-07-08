c7f5b7e8b9bebe505e82f1c6bfb98136b059891c
Mateo <matecuni@hotmail.com>
2025-07-08 17:50:31 -0300

    borro cosas que no llegue a hacer

--- consultas/models/Receta.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from .Consulta import Consulta
-
-class Receta(models.Model):
-    consulta = models.ForeignKey(Consulta, on_delete=models.CASCADE, related_name="recetas_asociadas")
-    documento = models.FileField(upload_to='recetas/')
