37fa1c866d93ed08d3aecff602d58a155ac1ac06
Mateo Cunille <matecuni@hotmail.com>
2025-10-26 21:04:23 -0300

    feat: Fix compatibility issues on linux

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 set -e
 
-mkdir -p /usr/local/app/media
-mkdir -p /usr/local/app/staticfiles
-chmod -p 777 /usr/local/app/media || true
-chmod -p 777 /usr/local/app/staticfiles || true
+mkdir -p /usr/local/app/staticfiles /usr/local/app/media
 
-python manage.py makemigrations users
-python manage.py makemigrations consultations
-python manage.py migrate --noinput
-python manage.py loaddata initial_specialties
-python manage.py collectstatic --no-input --clear || true
+python manage.py collectstatic --no-input
+python manage.py makemigrations
+python manage.py migrate --no-input
 
-exec gunicorn --bind 0.0.0.0:8000 --workers 3 config.wsgi:application
+exec "$@"
